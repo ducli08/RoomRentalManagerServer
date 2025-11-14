8f2368bb416384109be0818dbec148f25f7b1fab
ducli08 <leminhduc8821@gmail.com>
2025-11-14 11:04:49 +0700

    add base url

--- RoomRentalManagerServer.API/Program.cs
+++ RoomRentalManagerServer.API/Program.cs
@@ -63,7 +63,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: AllowSpecificOrigins,
         policy =>
         {
-            policy.WithOrigins("http://192.168.1.45:4200").AllowAnyHeader().AllowAnyMethod();
+            policy.WithOrigins(builder.Configuration["Base_URL"]).AllowAnyHeader().AllowAnyMethod();
         });
 });
 
